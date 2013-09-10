5f508b6a6d48d0980952357e0de55c18648601f4
syunta kitagawa <kitagawa@timedia.co.jp>
2013-09-10 11:15:10 +0900

    プログラム起動用のオブジェクト(クラス)StartApp、プログラムを運用するオブジェクト(クラス)BaseApp、イベント処理を管理するオブジェクト(クラス)EventHandlerを作成。

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-$(
-	function(){
-		$("body").text("hello JQuery");
+function StartApp(){
+	var app = new AppBase();
+	app.test();
+}
+
+function AppBase(){
+	this.test = function(){
+		var event = new EventHandler();
+		event.test();
 	}
-);
+}
+
+function EventHandler(){
+	this.test = function(){
+		$("body").text("this is a test.");
+	}
+}
