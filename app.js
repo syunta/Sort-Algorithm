e8338036e5aaf87aa6681da47e5fde3d319edab2
syunta kitagawa <kitagawa@timedia.co.jp>
2013-09-10 17:41:32 +0900

    UIAssembler、Layout、LayoutStyleを仮実装

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function UI(){
 	var assembler = new UIAssembler();
 	
 	this.draw = function(){
-		$("body").append( assembler.assembledPage );
+		assembler.assemble();
 	}
 }
 
@@ -21,20 +21,58 @@ function UIAssembler(){
 	var dom = new DOM();
 	var layout = new Layout();
 	
-	this.assembledPage = dom.$bubbleSortButton;
-	// TODO
+	this.assemble = function(){
+		assembleLayout();
+		assembleDOM();
+	}
+	
+	var assembleLayout = function(){
+		$("body").
+			prepend(layout.$header).
+			append(layout.$main).
+			append(layout.$footer);
+	}
+	
+	var assembleDOM = function(){
+		$("main").
+			append(dom.$bubbleSortButton);
+	}
 }
 
 function Layout(){
 	var css = new LayoutStyle();
 	
-	var $main = $("<main>");
-	
-	this.panorama = $main;
+	this.$header = css.defineHeader( $("<header>") );
+	this.$main = css.defineMain( $("<main>") );
+	this.$footer = css.defineFooter( $("<footer>") );
 }
 
 function LayoutStyle(){
-	// TODO
+	this.defineMain = function($main){
+		$main.css({
+			"width" :"100%",
+			"height" :"500px",
+		});
+		return $main;
+	}
+	
+	this.defineHeader = function($header){
+		$header.css({
+			"width" :"100%",
+			"height" :"50px",
+			"background-color" :"black",
+		});
+		return $header;
+	}
+	
+	this.defineFooter = function($footer){
+		$footer.css({
+			"width" :"100%",
+			"height" :"50px",
+			"background-color" :"black",
+		});
+		return $footer;
+	}
 }
 
 function DOM(){
@@ -55,7 +93,7 @@ function DOMStyle(){
 }
 
 $(function(){
-	$("#bubbleSort").click(function(){
-		$("body").text("hello");
+	$("*").click(function(){
+		$("body").html("hello");
 	});
 });
