cd888a3fd049c4c2cf3f9dad5a5c9599161d3959
syunta kitagawa <kitagawa@timedia.co.jp>
2013-09-20 17:47:44 +0900

    変数の先頭に付けていた$を外す

--- app.js
+++ app.js
@@ -28,77 +28,77 @@ function UIAssembler(){
 	
 	var assembleLayout = function(){
 		$("body").
-			prepend(layout.$header).
-			append(layout.$main).
-			append(layout.$footer);
+			prepend(layout.header).
+			append(layout.main).
+			append(layout.footer);
 		$("main").
-			prepend(layout.$buttonPlacement).
-			append(layout.$cardPlacement);
+			prepend(layout.buttonPlacement).
+			append(layout.cardPlacement);
 	}
 	
 	var assembleDOM = function(){
 		$(".buttonPlacement").
-			append(dom.$bubbleSortButton);
+			append(dom.bubbleSortButton);
 		$(".cardPlacement").
-			append(dom.$1);
+			append(dom.card1);
 	}
 }
 
 function Layout(){
 	var style = new LayoutStyle();
 	
-	this.$header = style.defineHeaderCSS( $("<header>") );
-	this.$main = style.defineMainCSS( $("<main>") );
-	this.$footer = style.defineFooterCSS( $("<footer>") );
-	this.$buttonPlacement = style.defineButtonPlacementCSS( $("<div>") );
-	this.$cardPlacement = style.defineCardPlacementCSS( $("<div>") );
+	this.header = style.defineHeaderCSS( $("<header>") );
+	this.main = style.defineMainCSS( $("<main>") );
+	this.footer = style.defineFooterCSS( $("<footer>") );
+	this.buttonPlacement = style.defineButtonPlacementCSS( $("<div>") );
+	this.cardPlacement = style.defineCardPlacementCSS( $("<div>") );
 }
 
 function LayoutStyle(){
 	
-	this.defineMainCSS = function($main){
-		$main.css({
+	this.defineMainCSS = function(main){
+		main.css({
 			"width": "100%",
 			"height": "500px",
 		});
-		return $main;
+		return main;
 	}
 	
-	this.defineHeaderCSS = function($header){
-		$header.css({
+	this.defineHeaderCSS = function(header){
+		header.css({
 			"width": "100%",
 			"height": "50px",
 			"background-color": "black",
 		});
-		return $header;
+		return header;
 	}
 	
-	this.defineFooterCSS = function($footer){
-		$footer.css({
+	this.defineFooterCSS = function(footer){
+		footer.css({
 			"width": "100%",
 			"height": "50px",
 			"background-color": "black",
 		});
-		return $footer;
+		return footer;
 	}
 	
-	this.defineButtonPlacementCSS = function($buttonPlacement){
-		$buttonPlacement.addClass("buttonPlacement");
-		$buttonPlacement.css({
+	this.defineButtonPlacementCSS = function(buttonPlacement){
+		buttonPlacement.addClass("buttonPlacement");
+		buttonPlacement.css({
 			"width": "100%",
 			"background-color": "gray",
 		});
-		return $buttonPlacement;
+		return buttonPlacement;
 	}
 	
-	this.defineCardPlacementCSS = function($cardPlacement){
-		$cardPlacement.addClass("cardPlacement");
-		$cardPlacement.css({
+	this.defineCardPlacementCSS = function(cardPlacement){
+		cardPlacement.addClass("cardPlacement");
+		cardPlacement.css({
 			"width": "100%",
 			"height":"100%",
 			"background-color": "#c0c0c0",
 		});
-		return $cardPlacement;
+		return cardPlacement;
 	}
 }
 
@@ -106,8 +106,8 @@ function DOM(){
 	var card = new Card();
 	var button = new Button();
 
-	this.$bubbleSortButton = button.create("BubbleSort");
-	this.$1 = card.create("1");
+	this.bubbleSortButton = button.create("BubbleSort");
+	this.card1 = card.create("1");
 }
 
 function Card(){
@@ -115,21 +115,21 @@ function Card(){
 	var style = new CardStyle();
 	
 	this.create = function(cardName){
-		var $card = style.defineCardCSS( basic.setup(cardName) );
-		return $card;
+		var card = style.defineCardCSS( basic.setup(cardName) );
+		return card;
 	}
 }
 
 function CardBasicSetup(){
 	this.setup = function(cardName){
-		var $card = $("<div>").text(cardName).attr("id",cardName);
-		return $card;
+		var card = $("<div>").text(cardName).attr("id",cardName);
+		return card;
 	}
 }
 
 function CardStyle(){
-	this.defineCardCSS = function($card){
-		$card.css({
+	this.defineCardCSS = function(card){
+		card.css({
 			"position":"relative",
 			"top":"30%",
 			"text-align":"center",
@@ -143,7 +143,7 @@ function CardStyle(){
 			"background-color":"white",
 			"border-radius":"15px",
 		});
-		return $card;
+		return card;
 	}
 }
 
@@ -152,25 +152,25 @@ function Button(){
 	var style = new ButtonStyle();
 
 	this.create = function(buttonName){
-		var $button = style.defineButtonCSS( basic.setup(buttonName) );
-		return $button;
+		var button = style.defineButtonCSS( basic.setup(buttonName) );
+		return button;
 	}
 }
 
 function ButtonBasicSetup(){
 	this.setup = function(buttonName){
-		var $button = $("<button>").text(buttonName).attr("id",buttonName);
-		return $button;
+		var button = $("<button>").text(buttonName).attr("id",buttonName);
+		return button;
 	}
 }
 
 function ButtonStyle(){
-	this.defineButtonCSS = function($button){
-		$button.css({
+	this.defineButtonCSS = function(button){
+		button.css({
 			"width" :"100px",
 			"height":"25px"
 		});
-		return $button;
+		return button;
 	}
 }
 
